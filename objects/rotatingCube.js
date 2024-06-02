881d5bfe950796dd1e150ac6d93ec8c9195e0259
LukaEaton <eatonluka@gmail.com>
2024-06-02 19:35:25 -0400

    first trial with rotating cube

--- objects/rotatingCube.js
+++ objects/rotatingCube.js
@@ -1,14 +1,3 @@
-import * as THREE from 'three';
-
-// Creating the Scene and Camera
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-
-// Creating the renderer and adding it to the HTML DOM
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
 // Creating our geometry and material, then merging it together into our shape, then adding the shape to the scene
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
